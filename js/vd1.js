73864f838abf67ee3d405b60f052978029fc94f0
Your Name <hoadang0727@gmail.com>
2022-09-26 13:38:08 +0700

    first commit

---
+++ js/vd1.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("hinh-ma").hover(function ()
+    {$(this).attr("scr", "images/love.jpeg");}, function() {{$(this).attr("scr", "images/love1.jpeg");}}
+    );
+});
